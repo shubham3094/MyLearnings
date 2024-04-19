6fe81343627f33032992092a98074757d932ef8e
shubham3094 <167502209+shubham3094@users.noreply.github.com>
2024-04-19 13:45:37 +0530

    New Class added

--- JavaCodePractice/src/CodePractice/ReverseEveryWordInSentence.java
+++ JavaCodePractice/src/CodePractice/ReverseEveryWordInSentence.java
@@ -4,8 +4,25 @@ public class ReverseEveryWordInSentence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String sen = "My name is shubham and I am an Engineer";
+		String revSen = "";
+		String[] arr = sen.split(" ");
+
+		for(int i=0;i<arr.length;i++) {
+
+			String revWord = "";
+			int len = arr[i].length()-1;
+			for(int j=len;j>=0;j--) {
+
+				revWord = revWord + arr[i].charAt(j);  
+
+			}
+			revSen = revSen + revWord+" ";
+
+		}
+
+		System.out.println(revSen);
 
 	}
 
