f448921e9e89d8a373d8bc40607fcf453eab0c7d
shubham3094 <167502209+shubham3094@users.noreply.github.com>
2024-04-20 13:15:19 +0530

    Series

--- JavaCodePractice/src/CodePractice/Factorial.java
+++ JavaCodePractice/src/CodePractice/Factorial.java
@@ -8,7 +8,7 @@ public class Factorial {
 		int num = 5;
 		int fact=1;
 		for(int i=1;i<=num;i++) {
-			
+
 			fact = fact*i;
 		}
 		System.out.println("the factorial of "+ num +" is : "+fact);
--- JavaCodePractice/src/CodePractice/Fibonacci.java
+++ JavaCodePractice/src/CodePractice/Fibonacci.java
@@ -5,20 +5,20 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+
 		int num = 10;
 		int first=0;
 		int second=1;
-		
-		
+
+
 		for(int i=1;i<=num;i++) {
-			
+
 			System.out.println( first+" ");
 			int third = first+second;
 			first=second;
 			second=third;
 		}
-		
+
 	}
 
 }
