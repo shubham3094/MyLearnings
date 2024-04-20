4ab41470617b4826f72c1b0368f33581655e448b
shubham3094 <167502209+shubham3094@users.noreply.github.com>
2024-04-20 13:09:00 +0530

    Series questions

---
+++ JavaCodePractice/src/CodePractice/Factorial.java
@@ -0,0 +1,17 @@
+package CodePractice;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 5;
+		int fact=1;
+		for(int i=1;i<=num;i++) {
+			
+			fact = fact*i;
+		}
+		System.out.println("the factorial of "+ num +" is : "+fact);
+	}
+
+}
