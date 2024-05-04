a049c2d39a96e938bbb02e543202284d25a659e3
shubham3094 <167502209+shubham3094@users.noreply.github.com>
2024-05-04 18:15:19 +0530

    done Framework part1

--- SeleniumBddProject/src/test/java/cucumberOptions/TestNgTestRunner.java
+++ SeleniumBddProject/src/test/java/cucumberOptions/TestNgTestRunner.java
@@ -6,7 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features="src/test/java/features",
 		glue= {"stepDefinitions"},
-		monochrome = true
+		monochrome = true,
+		tags="@Smoke or @Regression", // (will run with tags either smoke or regression)
+		//tags="@Smoke and @Regression"  (will run with both tags available smoke and regression)
+		//tags="not @Regression"  //(will run scenarios with No Regression tags)
+		plugin= {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"},
+		dryRun = true
 		)
 public class TestNgTestRunner extends AbstractTestNGCucumberTests{
 
